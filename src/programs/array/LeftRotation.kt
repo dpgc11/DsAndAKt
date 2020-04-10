23eab92d1579ff1ed4c8b4308523708ba680d053
dpgc11 <iamyogeshtelang@gmail.com>
2020-04-10 05:36:00 +0530

    feat: optimized LeftRotation so that it passes Hackerrank timed out exception

--- src/programs/array/LeftRotation.kt
+++ src/programs/array/LeftRotation.kt
@@ -1,23 +1,6 @@
 package programs.array
 
-import java.io.*
-import java.math.*
-import java.security.*
-import java.text.*
 import java.util.*
-import java.util.concurrent.*
-import java.util.function.*
-import java.util.regex.*
-import java.util.stream.*
-import kotlin.collections.*
-import kotlin.comparisons.*
-import kotlin.io.*
-import kotlin.jvm.*
-import kotlin.jvm.functions.*
-import kotlin.jvm.internal.*
-import kotlin.ranges.*
-import kotlin.sequences.*
-import kotlin.text.*
 
 
 fun main(args: Array<String>) {
@@ -35,24 +18,15 @@ fun main(args: Array<String>) {
 }
 
 fun leftRotate(_a: Array<Int>, _d: Int) {
-    val a = _a
+    val a = _a.toMutableList()
     val d = _d
     val iterations = d % a.size
-    println("iterations: $iterations")
-    for (j in 0 until iterations) {
-        val temp = a[a.size - 1]
-        a[a.size - 1] = a[0]
 
-        for (i in 0 until a.size - 1) {
-            a[i] = a[i + 1]
-        }
+    val tempArray1 = a.subList(iterations, a.size)
+    val tempArray = a.subList(0, iterations)
 
-        a[a.size - 2] = temp
-        println("while loop: j: $j")
-        a.forEach { print("$it  ") }
-        println()
-    }
+    tempArray1.addAll(tempArray)
 
-    print(a.joinToString(" "))
+    print(tempArray1.joinToString(" "))
 }
 
