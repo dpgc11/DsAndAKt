eb99514da30d550a65883e6ebd74d7e94baf87ee
dpgc11 <iamyogeshtelang@gmail.com>
2020-04-16 14:55:51 +0530

    feat: ReversePrint.kt using array

---
+++ src/programs/linkedList/ReversePrint.kt
@@ -0,0 +1,88 @@
+package programs.linkedList
+
+import java.util.*
+
+class SinglyLinkedListNode(nodeData: Int) {
+    var data: Int = nodeData
+    var next: SinglyLinkedListNode? = null
+}
+
+class SinglyLinkedList {
+    var head: SinglyLinkedListNode?
+    var tail: SinglyLinkedListNode?
+
+    init {
+        head = null
+        tail = null
+    }
+
+    fun insertNode(nodeData: Int) {
+        val node = SinglyLinkedListNode(nodeData)
+
+        if (head == null) {
+            head = node
+        } else {
+            tail?.next = node
+        }
+
+        tail = node
+    }
+
+}
+
+fun printSinglyLinkedList(head: SinglyLinkedListNode?, sep: String) {
+    var node = head
+
+    while (node != null) {
+        print(node.data)
+        node = node.next
+
+        if (node != null) {
+            print(sep)
+        }
+    }
+}
+
+// Complete the reversePrint function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data: Int
+ *     next: SinglyLinkedListNode
+ * }
+ *
+ */
+fun reversePrint(_llist: SinglyLinkedListNode?) {
+
+    var llist = _llist
+    val tempList = emptyList<Int>().toMutableList()
+
+    while (llist != null) {
+        tempList.add(llist.data)
+        llist = llist.next
+    }
+
+    tempList.reverse()
+
+    tempList.forEach { println("$it") }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val tests = scan.nextLine().trim().toInt()
+
+    for (testsItr in 1..tests) {
+        val llistCount = scan.nextLine().trim().toInt()
+        val llist = SinglyLinkedList()
+
+        for (i in 0 until llistCount) {
+            val llist_item = scan.nextLine().trim().toInt()
+            llist.insertNode(llist_item)
+        }
+
+        reversePrint(llist.head)
+    }
+}
