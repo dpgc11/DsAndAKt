bee99942bab6b4fcd83c56ace282beb1a4b0ebce
dpgc11 <iamyogeshtelang@gmail.com>
2020-04-20 17:25:32 +0530

    feat: get node from tail using 2 pointers method

--- src/programs/linkedList/GetNodeValue.kt
+++ src/programs/linkedList/GetNodeValue.kt
@@ -19,32 +19,58 @@ fun getNode(_llist: SinglyLinkedListNode?, positionFromTail: Int): Int {
     var llist = _llist
 
     /**
-     * First attempt start
+     * 2 pointer method START
      */
-    // 0 1 2 3 4    indexes
-    // 5 4 3 2 1    values
-    // size -  (positionFromTail + 1) from the start will give the item at positionFromTail when size is known
-    // 5 - (3 + 1) = 1
+    var main_ptr: SinglyLinkedListNode? = _llist
+    var ref_ptr: SinglyLinkedListNode? = _llist
+
     var count = 0
-    // find the size of the LL
-    while (llist != null) {
-        llist = llist.next
-        count++
+    if (_llist != null) {
+        while (count <= positionFromTail) {
+//            if (ref_ptr == null) {
+//                return
+//            }
+            ref_ptr = ref_ptr?.next
+            count++
+        }
+        while (ref_ptr != null) {
+            main_ptr = main_ptr!!.next
+            ref_ptr = ref_ptr.next
+        }
+        if (main_ptr != null)
+            result = main_ptr.data
     }
-    llist = _llist
-    // deduce the positionFromStart using the formula
-    val positionFromStart = count - (positionFromTail + 1)
-    println("positionFromStart: $positionFromStart")
-    // get the value at positionFromStart
-    for (i in 0 until positionFromStart) {
-        llist = llist?.next
-    }
-    // assign the value to result
-    if (llist != null) {
-        result = llist.data
-    }
+
     /**
-     * First attempt end
+     * END
+     */
+    /**
+     * Length of LL start
+     */
+//    // 0 1 2 3 4    indexes
+//    // 5 4 3 2 1    values
+//    // size -  (positionFromTail + 1) from the start will give the item at positionFromTail when size is known
+//    // 5 - (3 + 1) = 1
+//    var count = 0
+//    // find the size of the LL
+//    while (llist != null) {
+//        llist = llist.next
+//        count++
+//    }
+//    llist = _llist
+//    // deduce the positionFromStart using the formula
+//    val positionFromStart = count - (positionFromTail + 1)
+//    println("positionFromStart: $positionFromStart")
+//    // get the value at positionFromStart
+//    for (i in 0 until positionFromStart) {
+//        llist = llist?.next
+//    }
+//    // assign the value to result
+//    if (llist != null) {
+//        result = llist.data
+//    }
+    /**
+     * END
      */
 
     return result
